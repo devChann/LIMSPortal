0f3a516d222ed92cefdf47ed0ce08ac2929eaeeb
Elvis Ayiemba <ayiembaelvis@gmail.com>
2018-05-08 18:08:35 +0300

    UI Rework, removing unnecessary files

--- LIMSWebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LIMSWebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -81,7 +81,7 @@ namespace LIMSWebApp.Areas.Identity.Pages.Account
                     var callbackUrl = Url.Page(
                         "/Account/ConfirmEmail",
                         pageHandler: null,
-                        values: new { userId = user.Id, code = code },
+                        values: new { userId = user.Id, code },
                         protocol: Request.Scheme);
 
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
