3e863f4371836f9535d12c688db82bfdaaedfb93
Elvis Ayiemba <ayiembaelvis@gmail.com>
2019-02-21 23:31:46 +0300

    Create IParcel.cs

---
+++ LIMSInfrastructure/Repository/IParcel.cs
@@ -0,0 +1,21 @@
+﻿using LIMSCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIMSInfrastructure.Repository
+{
+	public interface IParcel
+	{
+		Parcel GetByNumber(string parcelNumber);
+		IEnumerable<Parcel> GetAll();
+		IEnumerable<Parcel> GetFilteredParcels(string searchQuery);
+		IEnumerable<Parcel> GetParcelsByOwner(string OwnerId);
+		Task Add(Parcel parcel);
+		Task Delete(string parcelNumber);
+		Task Update(string parcelNumber);
+
+
+	}
+}
