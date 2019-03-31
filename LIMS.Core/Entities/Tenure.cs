0324221a925ca56987b916ad5244810804110f14
elvis ayiemba <7754523+ayiemba@users.noreply.github.com>
2019-03-31 20:31:11 +0300

    adding razor pages

--- LIMS.Core/Entities/Tenure.cs
+++ LIMS.Core/Entities/Tenure.cs
@@ -6,7 +6,7 @@ namespace LIMS.Core.Entities
     public partial class Tenure
     {
         public Guid TenureId { get; set; }
-        public TenureType TenureType { get; set; } //can be leashold or freehold
+        public string TenureType { get; set; } //can be leashold or freehold
 		public DateTime TenureBeginDate { get; set; }
 		public DateTime TenureEndDate { get; set; }
 		public string Lessor { get; set; }
