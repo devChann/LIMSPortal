93c88ffe2d28b5849dc00fa2b1d972e82cae4c79
Elvis Ayiemba <ayiembaelvis@gmail.com>
2019-04-29 17:13:19 +0300

    minor

--- LIMS.WebApp/Areas/Admin/Pages/Dashboard.cshtml.cs
+++ LIMS.WebApp/Areas/Admin/Pages/Dashboard.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace LIMS.WebApp.Pages.Admin
+namespace LIMS.WebApp.Areas.Admin.Pages
 {
 
     public class DashboardModel : PageModel
